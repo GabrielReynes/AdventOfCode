a1b2cc11dd2bf4eb4187f45013f742de7814f765
Gabriel Reynes <gabrielreynes@gmail.com>
2021-12-02 17:55:27 +0100

    Day2

---
+++ 2021-2022/Day2/part2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  2 17:48:39 2021
+
+@author: Gabri
+"""
+
+lines = open("input.txt", 'r')
+aim, hor, depth = 0,0,0
+for l in lines:
+    c,u = l.split()
+    if c == 'forward':
+        hor += int(u)
+        depth += aim * int(u)
+    else :
+        aim += (['up','down'].index(c)*2-1) * int(u)
+print(hor*depth)
