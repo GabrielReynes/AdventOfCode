7652ff502a4a7d6595a16a2253fb0168ca89b51f
Gabriel Reynes <gabrielreynes@gmail.com>
2021-12-03 10:16:28 +0100

    day3

---
+++ 2021-2022/Day3/part1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  3 09:18:08 2021
+
+@author: Gabri
+"""
+
+lines = open('input.txt', 'r').readlines()
+res = [0] * len(lines[0])
+
+for l in lines:
+    res = list(sum(v) for v in zip(res, map(int, l.strip())))
+    
+gamma = int("".join(str(round(v/len(lines))) for v in res), 2)
+
+print(gamma * (~gamma & 0xFFF))
---
+++ 2021-2022/Day3/part2.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  3 09:43:49 2021
+
+@author: Gabri
+"""
+
+lines = open("input.txt", 'r').readlines()
+oxy = lines
+co2 = lines
+
+index = 0
+
+bit_count = lambda lst : lambda v: [t[index] for t in lst].count(v)
+
+while len(oxy) > 1 or len(co2) > 1:
+    if len(oxy) > 1:
+        recOxy = max(['1','0'], key=bit_count(oxy))
+        oxy = [v for v in oxy if v[index] == recOxy]
+    
+    if len(co2) > 1 :
+        recCo2 = min(['0','1'], key=bit_count(co2))
+        co2 = [v for v in co2 if v[index] == recCo2]
+        
+    index += 1
+    
+print(int(oxy[0],2) * int(co2[0],2))
