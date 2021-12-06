a881dd01af20284f3dca48e19f18d4260cbd5bc1
Gabriel Reynes <gabrielreynes@gmail.com>
2021-12-06 11:26:00 +0100

    day6

---
+++ 2021/Day6/part2.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  6 11:25:02 2021
+
+@author: Gabri
+"""
+
+tab = [0] * 9
+
+for i in map(int, open('input.txt','r').readline().split(',')): 
+    tab[i] += 1
+    
+for _ in range(256):
+    zero = tab[0]
+    for i in range(8):
+        tab[i] = tab[i+1]
+    tab[6] += zero
+    tab[8] = zero
+    
+print(sum(tab))
