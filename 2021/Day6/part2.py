f54bcbad745bd8015150d4b7cb2d788df45a5808
Gabriel Reynes <gabrielreynes@gmail.com>
2021-12-06 11:26:38 +0100

    format

--- 2021/Day6/part2.py
+++ 2021/Day6/part2.py
@@ -7,14 +7,14 @@ Created on Mon Dec  6 11:25:02 2021
 
 tab = [0] * 9
 
-for i in map(int, open('input.txt','r').readline().split(',')): 
+for i in map(int, open('input.txt', 'r').readline().split(',')):
     tab[i] += 1
-    
+
 for _ in range(256):
     zero = tab[0]
     for i in range(8):
         tab[i] = tab[i+1]
     tab[6] += zero
     tab[8] = zero
-    
+
 print(sum(tab))
