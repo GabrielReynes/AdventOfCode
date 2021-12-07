ed683535fa47b2e00b5156a55e2c47e9d21a9a6a
Gabriel Reynes <gabrielreynes@gmail.com>
2021-12-07 10:53:46 +0100

    format

--- 2021/Day7/part1.py
+++ 2021/Day7/part1.py
@@ -5,5 +5,5 @@ Created on Tue Dec  7 07:19:52 2021
 @author: Gabri
 """
 
-lst = sorted(map(int, open('test_input.txt','r').readline().split(',')))
+lst = sorted(map(int, open('test_input.txt', 'r').readline().split(',')))
 print(sum(abs(v-lst[len(lst)//2]) for v in lst))
--- 2021/Day7/part2.py
+++ 2021/Day7/part2.py
@@ -5,7 +5,7 @@ Created on Tue Dec  7 08:56:03 2021
 @author: Gabri
 """
 
-lst = tuple(map(int, open('input.txt','r').readline().split(',')))
-mean = sum(lst)/len(lst) #assuming that sum(lst)%len(lst) != 0
+lst = tuple(map(int, open('input.txt', 'r').readline().split(',')))
+mean = sum(lst)/len(lst)  # assuming that sum(lst)%len(lst) != 0
 mean += sum(v - int(mean) for v in lst if v > mean) > sum(int(mean)+1 - v for v in lst if v < mean)
 print(sum(abs(v-int(mean))*(abs(v-int(mean))+1)//2 for v in lst))
