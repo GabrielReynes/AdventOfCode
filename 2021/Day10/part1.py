6d08e24f91e87031915295d91d19b8df23d777fa
Gabriel Reynes <gabrielreynes@gmail.com>
2021-12-10 11:32:06 +0100

    day10

---
+++ 2021/Day10/part1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 10 09:44:17 2021
+
+@author: Gabri
+"""
+S = 0
+
+P = {')':('(', 3), ']':('[', 57), '}':('{', 1197), '>':('<', 25137)}
+
+for l in open('input.txt', 'r').readlines():
+    pile = []
+    for c in l:
+        if c not in P:
+            pile.append(c)
+        else:
+            if pile.pop() != P[c][0]:
+                S += P[c][1]
+                break
+print(S)
+        
+    
---
+++ 2021/Day10/part2.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 10 11:14:56 2021
+
+@author: Gabri
+"""
+SCORES = []
+
+O = ['(', '[', '{', '<']
+C = [')', ']', '}', '>']
+
+for l in open('input.txt', 'r').readlines():
+    pile = []
+    for c in l.strip():
+        if c in O:
+            pile.append(c)
+        elif pile.pop() != O[C.index(c)]:
+                break
+    else:
+        score = 0
+        while pile:
+            score = 5*score + O.index(pile.pop()) + 1
+        SCORES.append(score)
+    
+print(sorted(SCORES)[len(SCORES)//2])
+        
