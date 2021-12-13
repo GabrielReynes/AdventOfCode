0a7a0189087823dcd342fc7da5ddf6f7bd510a1e
Gabriel Reynes <gabrielreynes@gmail.com>
2021-12-13 14:32:24 +0100

    format

--- 2021/Day13/part1.py
+++ 2021/Day13/part1.py
@@ -9,10 +9,13 @@ f = open('input.txt')
 dots = set()
 
 for l in f:
-    if l.isspace(): break
-    dots.add(tuple(map(int, l.split(','))))
-    
+    if l.isspace():
+        break
+    dots.add(map(int, l.split(',')))
+
 axe, val = f.readline().split('=')
 
-if axe[-1]=='x': print(len(set((a if a<int(val) else 2*int(val)-a, b) for a,b in dots)))
-else: print(len(set((a, b if b<int(val) else 2*int(val)-b) for a,b in dots)))
+if axe[-1] == 'x':
+    print(len(set((a if a < int(val) else 2*int(val)-a, b) for a, b in dots)))
+else:
+    print(len(set((a, b if b < int(val) else 2*int(val)-b) for a, b in dots)))
