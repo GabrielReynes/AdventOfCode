e9b72b16887ffe302470bfb09506e8e4b27b10ee
Gabriel Reynes <gabrielreynes@gmail.com>
2021-12-14 08:54:02 +0100

    format

--- 2021/Day14/part2.py
+++ 2021/Day14/part2.py
@@ -10,15 +10,17 @@ lines = open('input.txt').readlines()
 starter = lines[0].strip()
 final = Counter(starter)
 counter = Counter(map(lambda i: tuple(starter[i:i+2]), range(len(starter)-1)))
-dic = {tuple(k.strip()) : v.strip() for k,v in map(lambda s:s.split('->'), lines[2:])}
+dic = {tuple(k.strip()): v.strip()
+       for k, v in map(lambda s: s.split('->'), lines[2:])}
 
 for _ in range(40):
     new = Counter()
-    for (a,b),v in counter.items():
-        add = dic[(a,b)]
+    for (a, b), v in counter.items():
+        add = dic[(a, b)]
         new[(a, add)] += v
         new[(add, b)] += v
         final[add] += v
     counter = new
-        
-print(max(final.values())-min(final.values()))
+
+count = final.values()
+print(max(count)-min(count))
