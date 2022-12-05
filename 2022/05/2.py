c81b2bf60360bb2cde7823974c7ad2daafc3cf7e
Gabriel Reynes <gabrielreynes@gmail.com>
2022-12-05 10:40:14 +0100

    Advent of Code 2022 Day 5

---
+++ 2022/05/2.py
@@ -0,0 +1,17 @@
+from sys import stdin
+import re
+
+starting, procedure = stdin.read().split('\n\n')
+
+arr = list(map(lambda l: ''.join(l).lstrip(),
+               zip(*map(lambda s: [s[i] for i in range(1, len(s), 4)], starting.split('\n')[:-1]))))
+
+for p in procedure.rstrip('\n').split('\n'):
+    nb, _from, to = map(int, re.search(r"move (\d+) from (\d+) to (\d+)", p).groups())
+    _from -= 1
+    to -= 1
+    take = arr[_from][:nb]
+    arr[_from] = arr[_from][nb:]
+    arr[to] = take + arr[to]
+
+print(''.join(stack[0] for stack in arr if stack))
