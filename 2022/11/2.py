f22b5e310b57861f4a8c510b3a87e709d90eb976
Gabriel Reynes <gabrielreynes@gmail.com>
2022-12-12 09:40:31 +0100

    Day 11 Optimization

--- 2022/11/2.py
+++ 2022/11/2.py
@@ -13,20 +13,16 @@ def monkey(description):
 
 
 monkeys = tuple(map(monkey, stdin.read().strip().split('\n\n')))
-divisors = tuple(map(itemgetter(2), monkeys))
+modulo = prod(map(itemgetter(2), monkeys))
 inspections = [0] * len(monkeys)
-
-for items, *args in monkeys:
-    for i, v in enumerate(items):
-        items[i] = [v % d for d in divisors]
 
 for _ in range(10_000):
     for m_idx, (items, update, divisor, true_idx, false_idx) in enumerate(monkeys):
         inspections[m_idx] += len(items)
         while items:
-            remainders = items.pop()
-            new = [eval(update) % divisors[i] for i, old in enumerate(remainders)]
-            idx = true_idx if new[m_idx] == 0 else false_idx
+            old = items.pop()
+            new = eval(update) % modulo
+            idx = true_idx if new % divisor == 0 else false_idx
             monkeys[idx][0].append(new)
 
 print(prod(nlargest(2, inspections)))
