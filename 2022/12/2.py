2958aa7ebe8c6d4fb0b4e5bba1b20cc93adf3ad4
Gabriel Reynes <gabrielreynes@gmail.com>
2022-12-12 22:57:59 +0100

    Request methods implementation

--- 2022/12/2.py
+++ 2022/12/2.py
@@ -1,27 +1,57 @@
-from sys import stdin
 from collections import deque
+from utils import aoc_utils
+
+DAY = 12
 
 S, E = None, None
 
-MAP = [[ord(c) if c.islower() else (S := (x, y)) if c == 'S' else (E := (x, y)) for x, c in enumerate(line)]
-       for y, line in enumerate(stdin.read().strip().split('\n'))]
+MAP = [[ord(c) - ord('a') if c.islower() else (S := (x, y)) if c == 'S' else (E := (x, y))
+        for x, c in enumerate(line)]
+       for y, line in enumerate(aoc_utils.get_input(12).strip().split('\n'))]
 
-MAP[S[1]][S[0]] = ord('a')
-MAP[E[1]][E[0]] = ord('z')
+MAP[S[1]][S[0]] = 0
+MAP[E[1]][E[0]] = 25
 
 W, H = len(MAP[0]), len(MAP)
 
-stack = deque([(0, E)])
+dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+stack = deque([(0, S)])
 seen = set()
 
-dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+while stack:
+    i, coords = stack.pop()
+
+    if coords == E:
+        break
+
+    if coords in seen:
+        continue
+
+    seen.add(coords)
+    x, y = coords
+    height = MAP[y][x]
+
+    for d in dirs:
+        nx, ny = x + d[0], y + d[1]
+
+        if 0 <= nx < W and 0 <= ny < H and MAP[ny][nx] - height < 2:
+            stack.appendleft((i + 1, (nx, ny)))
+
+print(i)
+aoc_utils.submit(DAY, 1, i)
+
+# PART 2
+
+stack = deque([(0, E)])
+seen = set()
 
 while stack:
     i, coords = stack.pop()
     x, y = coords
     height = MAP[y][x]
 
-    if height == ord('a'):
+    if height == 0:
         break
 
     if coords in seen:
@@ -33,6 +63,7 @@ while stack:
         nx, ny = x + d[0], y + d[1]
 
         if 0 <= nx < W and 0 <= ny < H and height - MAP[ny][nx] < 2:
-            stack.appendleft((i+1, (nx, ny)))
+            stack.appendleft((i + 1, (nx, ny)))
 
 print(i)
+aoc_utils.submit(DAY, 2, i)
